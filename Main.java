f13ae57a06b5582946e3930914da4344c5a8ecce
ipipi2024 <ipulepipi60@gmail.com>
2025-03-07 22:53:26 -0500

    added remove element method to the MaxHeap

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         MaxHeap maxHeap = new MaxHeap();
+        maxHeap.add(3);
         maxHeap.add(2);
         maxHeap.add(1);
-        maxHeap.add(0);
-        maxHeap.add(3);
+        maxHeap.remove();
+        maxHeap.remove();
+        maxHeap.remove();
+        maxHeap.remove();
+        
         maxHeap.printMaxHeap();
     }
 }
--- MaxHeap.java
+++ MaxHeap.java
@@ -15,11 +15,11 @@ public class MaxHeap {
         // add element
         maxHeap.add(num); 
         heapSize++;
-        swap(heapSize -1);
+        swapUp(heapSize -1);
     }
 
     //method to swap added element with parent so its in order
-    public void swap(int index) {
+    public void swapUp(int index) {
         
         if (index == 0) {
             return;
@@ -43,12 +43,79 @@ public class MaxHeap {
         Integer temp = parentElement;
         maxHeap.set(parentIndex, childElement);
         maxHeap.set(index, temp);
-        swap(parentIndex);
+        swapUp(parentIndex);
     }
 
     public void printMaxHeap () {
         for (int num: maxHeap) {
             System.out.print(num + " ");
         }
+    }
+
+    //method to remove element from root
+    public Integer remove() {
+        if(heapSize == 0) {
+            System.out.print("Heap is empty, cannot remove!");
+            return -1;
+        }
+        
+        Integer top = maxHeap.get(0);
+        Integer last = maxHeap.remove(heapSize -1);
+        if (heapSize == 1) {
+            heapSize--;
+            return top;
+        }
+        heapSize--;
+        maxHeap.set(0, last);
+       
+        swapDown(0);
+        return top;
+
+    }
+
+    //helper method to swap down  
+    public void swapDown(int index) {
+       
+        Integer parent = maxHeap.get(index);
+
+        
+        int leftChildIndex = 2 * index + 1;
+        
+        if (leftChildIndex >= heapSize) {
+            return;
+        }
+        int rightChildIndex = 2 * index + 2;
+        Integer leftChild = maxHeap.get(leftChildIndex);
+        
+
+        if (rightChildIndex >= heapSize) {
+            if (leftChild > parent) {
+                Integer temp = parent;
+                maxHeap.set(index, leftChild);
+                maxHeap.set(leftChildIndex, temp);
+                swapDown(leftChildIndex);
+                return;
+            }
+        }
+
+        Integer rightChild = maxHeap.get(rightChildIndex);
+        
+        
+        if (leftChild >= rightChild) {
+            if (leftChild > parent) {
+                Integer temp = parent;
+                maxHeap.set(index, leftChild);
+                maxHeap.set(leftChildIndex, temp);
+                swapDown(leftChildIndex);
+            }
+        }else{
+            if(rightChild > parent) {
+                Integer temp = parent;
+                maxHeap.set(index, rightChild);
+                maxHeap.set(rightChildIndex, temp);
+                swapDown(rightChildIndex);
+            }
+        }
+
     }
 }
